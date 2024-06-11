df408a445570bfe52d20b972fee3ef1b932d99b7
meirjc-adzz <meirco@adzz.com>
2024-06-11 11:31:43 +0300

    cards count changed to 180

--- src/routes/cupons/+page.server.js
+++ src/routes/cupons/+page.server.js
@@ -2,7 +2,7 @@ import { SECRET_DRCTS } from '$env/static/private'; // Api bearer token
 import { PUBLIC_DRCTS } from '$env/static/public';
 // console.log(SECRET_DRCTS);
 
-let url = 'https://cms.coupon.guru/items/coupons?limit=512';
+let url = 'https://cms.coupon.guru/items/coupons?limit=180';
 
 export async function load({ fetch }) {
 	try {
