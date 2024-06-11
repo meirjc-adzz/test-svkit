7d625ade648d3aa6be503c220e5201cfac75c3d9
meirjc-adzz <meirco@adzz.com>
2024-06-11 11:19:34 +0300

    added netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,11 +9,24 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+	// 	adapter: adapter()
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	}
 };
 
